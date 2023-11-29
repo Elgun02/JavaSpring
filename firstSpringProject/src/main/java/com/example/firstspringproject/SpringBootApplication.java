09bfe82f8d2f4c149726a5ff83fbbb4645fcbaa4
Elgun <eabdulaev12@mail.ru>
2023-11-29 12:34:58 +0000

    commit

---
+++ firstSpringProject/src/main/java/com/example/firstspringproject/SpringBootApplication.java
@@ -0,0 +1,43 @@
+package com.example.firstspringproject;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+@RestController
+public class SpringBootApplication {
+    private int count;
+    List<Integer> countList = new ArrayList<>();
+    @GetMapping(value = "/message")
+    private String message() {
+        String[] messages = {"мы вам не рады", "добро пожаловать"};
+        Random random = new Random();
+        int randomIndex = random.nextInt(messages.length);
+
+        return messages[randomIndex];
+    }
+
+    @GetMapping(value = "/count")
+    public void addCount() {
+        count++;
+    }
+
+    @GetMapping(value = "/info")
+    public int info() {
+        return count;
+    }
+
+    @GetMapping(value = "/save-and-reset")
+    public void saveAndResetCount() {
+        countList.add(count);
+        count = 0;
+    }
+
+    @GetMapping(value = "/list")
+    public List<Integer> getCountList() {
+        return countList;
+    }
+}
