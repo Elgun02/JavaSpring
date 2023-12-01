83aca89d1f791caa7278f81beff30ee20d12e455
Elgun <eabdulaev12@mail.ru>
2023-12-01 21:19:25 +0000

    commit

--- Lesson_2/src/main/java/com/example/lesson_2/Main.java
+++ Lesson_2/src/main/java/com/example/lesson_2/Main.java
@@ -6,7 +6,7 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
-@ImportResource("classpath:bean.xml")  // xml файл находися в папке "resources"
+@ImportResource("classpath:bean.xml")  // xml file -> "main/resources/bean.xml"
 public class Main {
 	public static void main(String[] args) {
 		ApplicationContext context = SpringApplication.run(Main.class, args);
